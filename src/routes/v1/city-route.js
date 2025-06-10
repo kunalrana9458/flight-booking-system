ac5ece958a6c12d9ad76121c4e4c426ef7fc2887
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-06-10 09:58:20 +0530

    Adding middleware for the city

--- src/routes/v1/city-route.js
+++ src/routes/v1/city-route.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {CityController} = require('../../controllers')
+const {CityMiddleware} = require('../../middlewares')
 
 
 // /api/v1/city - POST
 router.post('/',
+    CityMiddleware.validateCreateRequest,
     CityController.createCity)
 
 
