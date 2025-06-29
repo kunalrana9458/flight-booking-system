7988612c638d912672022796111348684a5aa4f7
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-06-29 21:18:13 +0530

    add filter for the sorting based in ascending or descending

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -27,6 +27,7 @@ async function createFlight(data) {
 async function getAllFlights(query){
   let customFilter = {}
   const endingTripTime = " 23:59:00"
+  let sortFilter = {}
   // trips=MUM-DEL
   if(query.trips){
     [departureAirportId,arrivalAirportId] = query.trips.split('-')
@@ -50,8 +51,13 @@ async function getAllFlights(query){
       [Op.between]: [query.tripDate,query.tripDate+endingTripTime ]
     }
   }
+  if(query.sort){
+    const params = query.sort.split(',')
+    const sortFilters = params.map((param) => param.split('_'))
+    sortFilter = sortFilters
+  }
   try {
-    const flights = await flightRepository.getAllFlights(customFilter)
+    const flights = await flightRepository.getAllFlights(customFilter,sortFilter)
     return flights;
   } catch (error) {
     throw new AppError('Cannot fetch data of all flights',StatusCodes.INTERNAL_SERVER_ERROR)
